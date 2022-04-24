3bbbc981ce64fc7a20a91d58aee0f8757e3e070b
Aaron Fleming <ercreeper@gmail.com>
2022-04-24 14:18:46 -0700

    Changed graph type to adjacency list. Added support for string node names.

--- bellman-ford.py
+++ bellman-ford.py
@@ -1,7 +1,11 @@
+from graphGenerate import Graph
+import string
+
+
 inf = float("inf")
 
 
-def bellman_ford(graph, start_index):
+def bellman_ford(g: Graph, start_name: string):
     """Computes the shortest path from an index to all other indexes using
     Bellman-Ford algorithm. This algorithm only returns the cost from the
     starting node to other nodes, but technically computes all shortest paths
@@ -9,7 +13,7 @@ def bellman_ford(graph, start_index):
     other results.
 
     Parameters:
-    graph (matrix): A graph represented in adjacency matrix form that can be
+    graph (matrix): A graph represented in adjacency list form that can be
         directed or undirected and must not contain any negative cycles.
     start_index (int): The index of the starting node in the graph.
 
@@ -21,18 +25,23 @@ def bellman_ford(graph, start_index):
         that must be traversed to get to the end node, including the starting
         node.
     """
-    graph_len = len(graph)
+    graph_len = len(g.graph)
     past_costs = [[inf] * graph_len for i in range(graph_len)]
     for i in range(graph_len):
         past_costs[i][i] = 0
-    shortest_paths = [[[i]] * graph_len for i in range(graph_len)]
+    shortest_paths = [[[f'{g.labelList[i]}']] * graph_len for i in range(graph_len)]
     
-    for i in range(graph_len - 2):
+    for k in range(graph_len - 2):
         curr_costs = past_costs.copy()
         # Updates estimated cost list by checking neighbor's cost lists
         for curr_index in range(graph_len):
-            for neighbor_index in range(graph_len):
-                edge_weight = graph[curr_index][neighbor_index]
+            num_neighbors = len(g.graph[curr_index])
+            for i in range(num_neighbors):
+                for j in range(len(g.labelList)):
+                    if (g.graph[curr_index][i][0] == g.labelList[j]):
+                        neighbor_index = j
+                        break
+                edge_weight = g.graph[curr_index][i][1]
                 # Check if the cost to neighbor's neighbor is less than current
                 for j in range(graph_len):
                     old_cost = past_costs[curr_index][j]
@@ -40,35 +49,60 @@ def bellman_ford(graph, start_index):
                     if (new_cost < old_cost):
                         # Update the cost and path
                         curr_costs[curr_index][j] = new_cost
-                        new_path = [curr_index]
+                        new_path = [g.labelList[curr_index]]
                         new_path.extend(shortest_paths[neighbor_index][j])
                         shortest_paths[curr_index][j] = new_path
         past_costs = curr_costs
     
+    for i in range(len(g.labelList)):
+        if (start_name == g.labelList[i]):
+            start_index = i
+            break
     return past_costs[start_index], shortest_paths[start_index]
 
 
 def bellman_ford_test():
-    inf = float("inf")
     print("--- Test 1 ---")
+    g = Graph(5)
     matrix = [[inf,   3,   1,   5, inf],
               [  3, inf, inf,   1, inf],
               [  1, inf, inf, inf,   9],
               [  5,   1, inf, inf,   2],
               [inf, inf,   9,   2, inf]]
-    costs, paths = bellman_ford(matrix, 0)
+    for i in range(len(matrix)):
+        g.labelList[i] = f'{i}'
+        for j in range(len(matrix[i])):
+            if (matrix[i][j] == inf):
+                continue
+            g.graph[i].append([f'{j}', matrix[i][j]])
+    costs, paths = bellman_ford(g, '0')
     print(costs) # [0, 3, 1, 4, 6]
-    print(paths) # [[0], [0, 1], [0, 2], [0, 1, 3], [0, 1, 3, 4]]
+    print(paths) # [['0'], ['0', '1'], ['0', '2'], ['0', '1', '3'], ['0', '1', '3', '4']]
 
     print("--- Test 2 ---")
+    g = Graph(4)
     matrix = [[inf,   4,   1, inf],
               [inf, inf,   1,   2],
               [inf,   1, inf, inf],
               [inf, inf, inf, inf]]
-    costs, paths = bellman_ford(matrix, 0)
+    for i in range(len(matrix)):
+        g.labelList[i] = f'{i}'
+        for j in range(len(matrix[i])):
+            if (matrix[i][j] == inf):
+                continue
+            g.graph[i].append([f'{j}', matrix[i][j]])
+    costs, paths = bellman_ford(g, '0')
     print(costs) # [0, 2, 1, 4]
-    print(paths) # [[0], [0, 2, 1], [0, 2], [0, 2, 1, 3]]
+    print(paths) # [['0'], ['0', '2', '1'], ['0', '2'], ['0', '2', '1', '3']]
 
+    print("--- Test 3 ---")
+    textPath="airports.txt"
+    cityList = Graph.citiesGen(textPath)
+    adjGraph=Graph(len(cityList))
+    adjGraph.populate(cityList)
+    costs, paths = bellman_ford(adjGraph, 'SJC')
+    print(costs)
+    print(paths)
 
 if (__name__ == "__main__"):
     bellman_ford_test()
--- dijkstra.py
+++ dijkstra.py
@@ -1,15 +1,17 @@
+import string
 import fibheap
+from graphGenerate import Graph
 
 
 inf = float("inf")
 
 
-def dijkstra(graph, start_index):
+def dijkstra(g: Graph, start_name: string):
     """Computes the shortest path from an index to all other indexes using
     dijkstra's algorithm.
 
     Parameters:
-    graph (matrix): A graph represented in adjacency matrix form that can be
+    graph (matrix): A graph represented in adjacency list form that can be
         directed or undirected and must not contain any negative edges.
     start_index (int): The index of the starting node in the graph.
 
@@ -21,16 +23,16 @@ def dijkstra(graph, start_index):
         that must be traversed to get to the end node, including the starting
         node.
     """
-    graph_len = len(graph)
+    graph_len = len(g.graph)
     unsure_nodes = {}
     find_nodes = {}
     dist = fibheap.Fheap()
     shortest_estimate = [None] * graph_len
-    shortest_paths = [[start_index]] * graph_len
+    shortest_paths = [[start_name]] * graph_len
 
     # Build fibonacci heap to store distances and unsure node list
     for i in range(graph_len):
-        if (i == start_index):
+        if (g.labelList[i] == start_name):
             newNode = fibheap.Node(0)
         else:
             newNode = fibheap.Node(inf)
@@ -44,19 +46,22 @@ def dijkstra(graph, start_index):
         curr_index = unsure_nodes[f"{curr_node}"]
         curr_cost = curr_node.key
         curr_path = shortest_paths[curr_index]
+        num_neighbors = len(g.graph[curr_index])
 
         # Calculate the new cost to the neighbor and compare it against old cost
-        for neighbor_index in range(graph_len):
-            if (neighbor_index == curr_index):
-                continue
+        for i in range(num_neighbors):
+            for j in range(len(g.labelList)):
+                if (g.graph[curr_index][i][0] == g.labelList[j]):
+                    neighbor_index = j
+                    break
             neighbor_node = find_nodes[f"{neighbor_index}"]
             curr_neighbor_cost = neighbor_node.key
-            new_neighbor_cost = curr_cost + graph[curr_index][neighbor_index]
+            new_neighbor_cost = curr_cost + g.graph[curr_index][i][1]
             if (new_neighbor_cost < curr_neighbor_cost):
                 # Update the cost and path
                 dist.decrease_key(neighbor_node, new_neighbor_cost)
                 new_path = curr_path.copy()
-                new_path.append(neighbor_index)
+                new_path.append(g.labelList[neighbor_index])
                 shortest_paths[neighbor_index] = new_path
         
         # Mark current node as sure and save its cost
@@ -68,24 +73,46 @@ def dijkstra(graph, start_index):
 
 def dijkstra_test():
     print("--- Test 1 ---")
+    g = Graph(5)
     matrix = [[inf,   3,   1,   5, inf],
               [  3, inf, inf,   1, inf],
               [  1, inf, inf, inf,   9],
               [  5,   1, inf, inf,   2],
               [inf, inf,   9,   2, inf]]
-    costs, paths = dijkstra(matrix, 0)
+    for i in range(len(matrix)):
+        g.labelList[i] = f'{i}'
+        for j in range(len(matrix[i])):
+            if (matrix[i][j] == inf):
+                continue
+            g.graph[i].append([f'{j}', matrix[i][j]])
+    costs, paths = dijkstra(g, '0')
     print(costs) # [0, 3, 1, 4, 6]
-    print(paths) # [[0], [0, 1], [0, 2], [0, 1, 3], [0, 1, 3, 4]]
+    print(paths) # [['0'], ['0', '1'], ['0', '2'], ['0', '1', '3'], ['0', '1', '3', '4']]
 
     print("--- Test 2 ---")
+    g = Graph(4)
     matrix = [[inf,   4,   1, inf],
               [inf, inf,   1,   2],
               [inf,   1, inf, inf],
               [inf, inf, inf, inf]]
-    costs, paths = dijkstra(matrix, 0)
+    for i in range(len(matrix)):
+        g.labelList[i] = f'{i}'
+        for j in range(len(matrix[i])):
+            if (matrix[i][j] == inf):
+                continue
+            g.graph[i].append([f'{j}', matrix[i][j]])
+    costs, paths = dijkstra(g, '0')
     print(costs) # [0, 2, 1, 4]
-    print(paths) # [[0], [0, 2, 1], [0, 2], [0, 2, 1, 3]]
+    print(paths) # [['0'], ['0', '2', '1'], ['0', '2'], ['0', '2', '1', '3']]
 
+    print("--- Test 3 ---")
+    textPath="airports.txt"
+    cityList = Graph.citiesGen(textPath)
+    adjGraph=Graph(len(cityList))
+    adjGraph.populate(cityList)
+    costs, paths = dijkstra(adjGraph, 'SJC')
+    print(costs)
+    print(paths)
 
 if (__name__ == "__main__"):
     dijkstra_test()
